71d0632382bf575790ce60097819f10ab9ab9fd1
James Gelok <jdgelok@gmail.com>
2021-02-27 18:43:18 -0500

    added dockerfile

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const stringifyObject = require("stringify-object");
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("*", (req, res) => {
   res.send(`Hello, ${stringifyObject(req.url)}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
